1853920af23b4bb0e61381daaadc328dfdb0be93
zervis <zervis@gmail.com>
2022-05-16 19:55:55 +0200

    Update emanuel.py

--- emanuel.py
+++ emanuel.py
@@ -31,4 +31,6 @@ def ask(question, chat_log=None):
  return str(story)
 
 def append_interaction_to_chat_log(question, answer, chat_log=None):
-  if chat_log is None: chat_log = session_prompt return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
