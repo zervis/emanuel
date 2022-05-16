59882b1e84556dc557b1879efc17f85fbfd21f35
zervis <zervis@gmail.com>
2022-05-16 20:16:28 +0200

    Update emanuel.py

--- emanuel.py
+++ emanuel.py
@@ -4,8 +4,7 @@ from flask import Flask, request
 import os
 import openai
 load_dotenv()
-#openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.api_key = "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"
+openai.api_key = os.getenv('OPENAI_API_KEY')
 completion = openai.Completion()
 
 start_sequence = "\nEmanuel:"
