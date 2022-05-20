9ab30d15d2bd49d8d65cc859b172aa99de70f415
zervis <zervis@gmail.com>
2022-05-20 12:59:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from emanuel import ask, append_interaction_to_chat_log
 app = Flask(__name__)
 # if for some reason your conversation with Emanuel gets weird, change the secret key
-app.config['SECRET_KEY'] = 'qwerty'
+app.config['SECRET_KEY'] = 'secret1'
 @app.route('/emanuel', methods=['POST'])
 def emanuel():
  incoming_msg = request.values['Body']
